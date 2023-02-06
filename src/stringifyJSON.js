93795e789a91d19bbb72a78e2c1cf260c17c2af3
Will Miller <millerw4@miamioh.edu>
2023-02-06 16:33:04 -0500

    pseudocode for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,18 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // I - any kind of object (array, string object literal etc)
+  // O - a string representing the object formatted according to specific rules
+  // C - do not use JSON.stringify
+  // E - undefined, functions = null
+
+  // initialize empty string variable to accumulate to
+  // determine what type of object we have
+  // handle stringification based on type of object
+
+  // if object literal, needs curly brace at beginning and end of string
+  // each key will have to be in double quotes
+  // colon, followed by the value
+
+  // TODO: consider joining elements to an array or other collection
 };
